e2874947f612b1bf6f3c45d275490ba4c0003ce6
Marcio Bueno <marciobueno@gmail.com>
2024-09-30 20:54:39 -0300

    método DELETE associado ao back4app

--- app-005/src/contacts.js
+++ app-005/src/contacts.js
@@ -54,34 +54,9 @@ export async function updateContact(id, updates) {
 }
 
 export async function deleteContact(id) {
-  let contacts = await localforage.getItem("contacts");
-  let index = contacts.findIndex((contact) => contact.id === id);
-  if (index > -1) {
-    contacts.splice(index, 1);
-    await set(contacts);
-    return true;
-  }
-  return false;
-}
-
-function set(contacts) {
-  return localforage.setItem("contacts", contacts);
-}
-
-// fake a cache so we don't slow down stuff we've already seen
-let fakeCache = {};
-
-async function fakeNetwork(key) {
-  if (!key) {
-    fakeCache = {};
-  }
-
-  if (fakeCache[key]) {
-    return;
-  }
-
-  fakeCache[key] = true;
-  return new Promise((res) => {
-    setTimeout(res, Math.random() * 800);
+  const response = await fetch(`${baseURL}/${id}`, {
+    method: "DELETE",
+    headers: headers,
   });
+  return response.ok;
 }
